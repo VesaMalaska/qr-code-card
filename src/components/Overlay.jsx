b824f050fb9aa0c16658acf2f1730b2b645639c4
VesaMalaska <vesa.malaska@gmail.com>
2023-03-21 15:18:20 +0200

    tweaked min height styles

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 export default function Overlay() {
     return (
-        <div className="flex justify-center items-center min-h-screen min-h-[100svh] bg-overlay-bg">
+        <div className="flex justify-center items-center min-h-[100svh] bg-overlay-bg">
             <Card />
         </div>
     );
